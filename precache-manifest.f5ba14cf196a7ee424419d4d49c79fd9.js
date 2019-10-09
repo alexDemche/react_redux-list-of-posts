779be1feea4fd37709693ac444e46f4d6d830b1b
alexDemche <alexeyydemchenko@gmail.com>
2019-10-09 21:45:51 +0300

    add build versioa

---
+++ precache-manifest.f5ba14cf196a7ee424419d4d49c79fd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90fe0d2760ad457981d71d2d6670084",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd8fee5158c40e210bf8",
+    "url": "./static/css/main.bb7b99ab.chunk.css"
+  },
+  {
+    "revision": "7358a1b07510be968943",
+    "url": "./static/js/2.63f1df51.chunk.js"
+  },
+  {
+    "revision": "cd8fee5158c40e210bf8",
+    "url": "./static/js/main.951d6df1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
